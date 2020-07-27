a2ee86112e015623e9ca3b4c940e2e8758a27896
inakaushi <38320788+inakaushi@users.noreply.github.com>
2020-07-27 10:37:28 +0900

    Script 追加

---
+++ Assets/Scripts/MapCsvImporterEditor.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(MapCsvImporter))]
+public class MapCsvImporterEditor : Editor
+{
+	public override void OnInspectorGUI()
+	{
+		DrawDefaultInspector();
+
+		if (GUILayout.Button("マップデータの作成"))
+		{
+			Debug.Log("マップデータの作成ボタンが押された");
+		}
+	}
+}
