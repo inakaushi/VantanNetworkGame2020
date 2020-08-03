ea30e27fabce521b5aebc6f60103e2cf39e60b9c
watakou <kaizokuou@icloud.com>
2020-08-03 13:21:43 +0900

    Trap選択画面のボタンを押してTrap取得、まずは名前から

---
+++ Assets/WatanabeFolder/SelectTrapDate.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class SelectTrapDate
+{
+    private Sprite trapImage;
+
+    private string trapName;
+
+    public SelectTrapDate(string name)
+    {
+        trapName = name;
+    }
+
+    public string GetTrapName() 
+    {
+        return trapName;
+    }
+}
---
+++ Assets/WatanabeFolder/StockTrap.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StockTrap : MonoBehaviour
+{
+    public List<SelectTrapDate> selectTrapList = new List<SelectTrapDate>();
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void SetTraps(string name) 
+    {
+        selectTrapList.Add(new SelectTrapDate(name));
+        Debug.Log(name + "ゲット");
+    } 
+}
