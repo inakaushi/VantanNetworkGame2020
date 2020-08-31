642cb1f4b35f0b462207d1d0aff24db50a18f968
Your Name <dai ryukawa>
2020-08-31 12:21:52 +0900

    トラップセット時にカメラがマウスの位置に応じて移動する処理を実装

--- Assets/Ryukawa/Scripts/CameraController.cs
+++ Assets/Ryukawa/Scripts/CameraController.cs
@@ -4,9 +4,19 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
+	enum CAMERA_CONTROL_MODE
+	{
+		TARGET_PLAYER,
+		TRAP_SET,
+	}
+
+	CAMERA_CONTROL_MODE controlType = CAMERA_CONTROL_MODE.TARGET_PLAYER;
+
+	[SerializeField] float widthMoveCamera = 10f;
     [SerializeField] Vector3 offsetPos = Vector3.zero;
     [SerializeField] Vector3 angle = Vector3.zero;
     [SerializeField] float speed = 5f;
+	[SerializeField] float trapSetModeMoveSpeed = 10f;
 
     Transform target = null;
 
@@ -17,10 +27,20 @@ public class CameraController : MonoBehaviour
 
 	void LateUpdate()
 	{
-        CameraMove(Time.deltaTime);
+		switch (controlType)
+		{
+			case CAMERA_CONTROL_MODE.TARGET_PLAYER:
+				TargetPlayerMove(Time.deltaTime);
+				break;
+			case CAMERA_CONTROL_MODE.TRAP_SET:
+				TrapSetCameraMove(Time.deltaTime);
+				break;
+			default:
+				break;
+		}
 	}
 
-    void CameraMove(float dt)
+    void TargetPlayerMove(float dt)
 	{
 		if (target)
 		{
@@ -29,8 +49,38 @@ public class CameraController : MonoBehaviour
 		}
 	}
 
+	void TrapSetCameraMove(float dt)
+	{
+		var mousePos = Input.mousePosition;
+		var pos = transform.localPosition;
+		if (mousePos.x >= Screen.width - widthMoveCamera)
+		{
+			pos += Vector3.right; 
+		}
+		if (mousePos.x <= 0 + widthMoveCamera)
+		{
+			pos += Vector3.left;
+		}
+		if (mousePos.y >= Screen.height - widthMoveCamera)
+		{
+			pos += Vector3.forward;
+		}
+		if (mousePos.y <= 0 + widthMoveCamera)
+		{
+			pos += Vector3.back;
+		}
+		transform.localPosition = Vector3.Lerp(transform.localPosition, pos, speed * dt);
+	}
+
     public void SetTarget(Transform target)
 	{
+		controlType = CAMERA_CONTROL_MODE.TARGET_PLAYER;
         this.target = target;
+	}
+
+	public void StartTrapSetMode()
+	{
+		if (controlType != CAMERA_CONTROL_MODE.TRAP_SET)
+			controlType = CAMERA_CONTROL_MODE.TRAP_SET;
 	}
 }
