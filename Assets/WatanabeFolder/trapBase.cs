a4d556d0d6e609df30ff1ce4c8ea67db23dd3a35
watakou <kaizokuou@icloud.com>
2020-08-31 11:54:47 +0900

    棘罠、落とし穴の実装。

---
+++ Assets/WatanabeFolder/trapBase.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrapBase : MonoBehaviour
+{
+    private bool trapState = false;
+
+    public void TrapOn() 
+    {
+        trapState = true;
+    }
+
+    public void TrapOff()
+    {
+        trapState = false;
+    }
+
+    public bool NowTrapState() 
+    {
+        return trapState;
+    }
+}
